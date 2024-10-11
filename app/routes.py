8cb32e4b5fcd9b477c9327743e06a05b4d6f40e4
Caio Machado <caio.terto@souunit.com.br>
2024-10-11 17:21:23 -0300

    requirements venv

--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,10 @@
 from flask import Flask, request, render_template, redirect, url_for, flash
-from werkzeug.utils import secure_filename
-import os
 import google.generativeai as genai
+import os
 from dotenv import load_dotenv
-from PyPDF2 import PdfReader  
+from PyPDF2 import PdfReader
 import docx
+import io
 
 # Carrega as variáveis de ambiente com a chave da API
 load_dotenv()
@@ -13,29 +13,25 @@ genai.configure(api_key=chave)
 
 # Inicializa a aplicação Flask
 app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = 'uploads/'  # Pasta onde os arquivos serão salvos
 app.secret_key = 'supersecretkey'  # Defina uma chave para os flash messages
 
-# Garante que a pasta de uploads exista
-os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
-
 # Função para extrair texto de arquivos PDF
-def extract_text_from_pdf(file_path):
-    reader = PdfReader(file_path)
+def extract_text_from_pdf(file_stream):
+    reader = PdfReader(file_stream)
     text = ""
     for page in reader.pages:
         text += page.extract_text()
     return text
 
 # Função para extrair texto de arquivos DOCX
-def extract_text_from_docx(file_path):
-    doc = docx.Document(file_path)
+def extract_text_from_docx(file_stream):
+    doc = docx.Document(file_stream)
     text = ""
     for paragraph in doc.paragraphs:
         text += paragraph.text
     return text
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/precorrecao', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
         # Captura o texto do textarea
@@ -49,19 +45,13 @@ def index():
             flash('É necessário fornecer a fundamentação legal e anexar um arquivo.', 'error')
             return redirect(url_for('index'))
 
-        # Salva o arquivo de forma segura
-        filename = secure_filename(file.filename)
-        file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-        file.save(file_path)
-        
-        # Lê o conteúdo do arquivo com base no tipo de arquivo
+        # Lê o conteúdo do arquivo com base no tipo de arquivo diretamente da memória
         if file.filename.endswith('.pdf'):
-            arquivo_texto = extract_text_from_pdf(file_path)
+            arquivo_texto = extract_text_from_pdf(io.BytesIO(file.read()))
         elif file.filename.endswith('.docx'):
-            arquivo_texto = extract_text_from_docx(file_path)
+            arquivo_texto = extract_text_from_docx(io.BytesIO(file.read()))
         elif file.filename.endswith('.txt'):
-            with open(file_path, 'r', encoding='utf-8') as f:
-                arquivo_texto = f.read()
+            arquivo_texto = file.read().decode('utf-8')
         else:
             flash('Tipo de arquivo não suportado. Apenas PDF, DOCX e TXT são permitidos.', 'error')
             return redirect(url_for('index'))
