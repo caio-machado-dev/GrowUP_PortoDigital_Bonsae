a6010ee171286b32ae6b3d948c587b8ab59a95d7
Caio Machado <caio.terto@souunit.com.br>
2024-11-27 22:35:43 -0300

    alteração código para hospedagem

--- setup/settings.py
+++ setup/settings.py
@@ -26,7 +26,6 @@ SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG", cast=bool, default=False)
-DEBUG = True
 
 ALLOWED_HOSTS = config("ALLOWED_HOSTS", cast=Csv())
 
